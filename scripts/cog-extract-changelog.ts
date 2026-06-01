6d845b3f970537aa8b146e12ff03b5a7bf98b7a4
Brad Jones <brad@bjc.id.au>
2026-06-01 17:00:04 +1000

    style(changelog): actually I changed my mind, lets just not format the changelog at all

--- scripts/cog-extract-changelog.ts
+++ scripts/cog-extract-changelog.ts
@@ -8,5 +8,5 @@ import { toText } from "@std/streams";
 */
 
 const input = await toText(Deno.stdin.readable);
-const output = input.split("---")[1]?.trim() + "\n";
+const output = input.split("- - -")[1]?.trim() + "\n";
 await Deno.stdout.write(new TextEncoder().encode(output));
